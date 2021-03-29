7f1c4726c85df8ee78abdb6e950eef489133d92f
Anna <annasole.ca@gmail.com>
2021-03-29 16:55:37 +0200

    DONE: algorithm that returnes combined overlapping intervals, TO DO: add comments

--- combined_intervals_checking_v2.py
+++ combined_intervals_checking_v2.py
@@ -18,4 +18,19 @@ strings_to_datetime(Intervals1)
 strings_to_datetime(Intervals2)
 
 merged = pd.merge_ordered(Intervals1, Intervals2,
-              on=['dia', 'hora inici', 'hora final'])
+              on=['dia', 'hora inici', 'hora final', 'resultat'])
+def join_times(x):
+    startdf = pd.DataFrame({'time':x['hora inici'], 'what':1})
+    enddf = pd.DataFrame({'time':x['hora final'], 'what':-1})
+    mergdf = pd.concat([startdf, enddf]).sort_values('time')
+    mergdf['running'] = mergdf['what'].cumsum()
+    mergdf['newwin'] = mergdf['running'].eq(1) & mergdf['what'].eq(1)
+    mergdf['group'] = mergdf['newwin'].cumsum()
+    x['group'] = mergdf['group'].loc[mergdf['what'].eq(1)]
+    return mergdf
+
+mergdf = join_times(merged)
+grouped_df = merged.groupby("group")
+combined = pd.DataFrame()
+combined= grouped_df.min()
+combined['hora final'] = grouped_df['hora final'].max()
